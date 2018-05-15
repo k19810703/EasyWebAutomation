fff14742ccffd434a06700887f559ca52359c563
HuiDong Wu <wuhd@cn.ibm.com>
2018-05-15 23:31:34 +0800

    edit

--- ui/builduiimage.sh
+++ ui/builduiimage.sh
@@ -6,6 +6,10 @@ rm -rf ./chanceauto/node_modules
 rm -rf ./chanceauto/.idea
 rm -rf ./chanceauto/output
 mkdir ./chanceauto/output
-docker build -t blockchain-03.cn.ibm.com:83/library/webautoui:$1 --no-cache .
-docker push blockchain-03.cn.ibm.com:83/library/webautoui:$1
+docker build -t registry.ng.bluemix.net/mycontainter/webautoui:$1 --no-cache .
+bx login -a https://api.ng.bluemix.net --apikey 3LPfOEKqJQ1x98R4UmrlPSRutBDoE7wF7aloP-P5VdFN
+bx target --cf
+bx cr login
+
+docker push registry.ng.bluemix.net/mycontainter/webautoui:$1
 rm -rf ./chanceauto
